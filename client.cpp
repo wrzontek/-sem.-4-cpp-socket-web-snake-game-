bc4eb23ad03864e78973b1b8190a1119745a4952
wrzontek <matwiejuk.adrian@gmail.com>
2021-05-23 18:14:28 +0200

    ameno

--- client.cpp
+++ client.cpp
@@ -5,6 +5,7 @@
 #include <cstring>
 #include <algorithm>
 #include <netdb.h>
+#include <unistd.h>
 #include "crc.h"
 #include "common.h"
 
@@ -13,10 +14,13 @@
 namespace {
     int server_sock, gui_sock;
     std::string player_name;
-    uint16_t server_port = DEFAULT_SERVER_PORT;
-    uint16_t gui_port = DEFAULT_GUI_PORT;
-    char default_gui_addr[] = "localhost";
+    char *server_port = NULL;
+    char *gui_port = NULL;
     char *gui_addr_arg = NULL;
+    char default_gui_addr[] = "localhost";
+    char default_server_port[] = "2021";
+    char default_gui_port[] = "20210";
+
     // address serwera, gui
 
 
@@ -33,25 +37,31 @@ namespace {
                             fatal("invalid characters in player name");
                     break;
                 case 'p':
-                    server_port = atoi(optarg);
+                    server_port = optarg;
                     break;
                 case 'i':
                     gui_addr_arg = optarg;
                     break;
                 case 'r':
-                    gui_port = atoi(optarg);
+                    gui_port = optarg;
                     break;
                 default:
                     fatal("Arguments: game_server [-n player_name] [-p n] [-i gui_server] [-r n]\n");
             }
         }
+        if (gui_addr_arg == NULL)
+            gui_addr_arg = default_gui_addr;
+        if (server_port == NULL)
+            server_port = default_server_port;
+        if (gui_port == NULL)
+            gui_port = default_gui_port;
     }
 
-    void init_hints(addrinfo &addr_hints) {
+    void init_hints(addrinfo &addr_hints, int protocol, int sock_type) {
         (void) memset(&addr_hints, 0, sizeof(struct addrinfo));
-        addr_hints.ai_family = AF_INET; // IPv6
-        addr_hints.ai_socktype = SOCK_DGRAM;
-        addr_hints.ai_protocol = IPPROTO_UDP;
+        addr_hints.ai_family = AF_UNSPEC;
+        addr_hints.ai_socktype = sock_type;
+        addr_hints.ai_protocol = protocol;
         addr_hints.ai_flags = 0;
         addr_hints.ai_addrlen = 0;
         addr_hints.ai_addr = NULL;
@@ -64,57 +74,48 @@ int main(int argc, char *argv[]) {
     if (argc < 2)
         fatal("Arguments: game_server [-n player_name] [-p n] [-i gui_server] [-r n]\n");
 
-    addrinfo addr_hints_server, addr_hints_gui;
+    addrinfo addr_hints_server{}, addr_hints_gui{};
     addrinfo *addr_server_result, *addr_gui_result;
 
-    sockaddr_in server_address;
-    sockaddr_in gui_address;
-    socklen_t rcva_len;
-    size_t len;
+//    sockaddr_in6 server_address;
+//    sockaddr_in6 gui_address;
     player_name = ""; // puste oznacza obserwatora
 
     get_args(argc - 1, argv + 1);
 
-    if (gui_addr_arg == NULL)
-        gui_addr_arg = default_gui_addr;
+    init_hints(addr_hints_server, IPPROTO_UDP, SOCK_DGRAM);
 
-    init_hints(addr_hints_server);
-    if (getaddrinfo(argv[1], NULL, &addr_hints_server, &addr_server_result) != 0)
+    if (getaddrinfo(argv[1], server_port, &addr_hints_server, &addr_server_result) != 0)
         syserr("getaddrinfo server");
-
-    init_hints(addr_hints_gui);
-    if (getaddrinfo(gui_addr_arg, NULL, &addr_hints_gui, &addr_gui_result) != 0)
-        syserr("getaddrinfo gui");
-
-    server_address.sin_family = AF_INET; // IPv6
-    server_address.sin_addr.s_addr =
-            ((struct sockaddr_in*) (addr_server_result->ai_addr))->sin_addr.s_addr;
-    server_address.sin_port = htons(server_port);
+    
+    server_sock = socket(addr_server_result->ai_family,
+                         addr_server_result->ai_socktype,
+                         addr_server_result->ai_protocol);
+    if (server_sock < 0)
+        syserr("socket server");
+    
+    if (connect(server_sock, addr_server_result->ai_addr, addr_server_result->ai_addrlen))
+        syserr("connect server");
 
     freeaddrinfo(addr_server_result);
 
-    gui_address.sin_family = AF_INET; // IPv6
-    gui_address.sin_addr.s_addr =
-            ((struct sockaddr_in*) (addr_gui_result->ai_addr))->sin_addr.s_addr;
-    gui_address.sin_port = htons(gui_port);
+//    char buf[10] = "012345678";
+//    write(server_sock, buf, 10);
 
-    freeaddrinfo(addr_gui_result);
-    
-    std::cout << " sport: " << server_port << " gport: " << gui_port << std::endl;
-    std::cout << "name: " << player_name << " serverport: " << ntohs(server_address.sin_port)
-    << " guiport: " << ntohs(gui_address.sin_port) << std::endl;
-    std::cout << " serveraddr: " << server_address.sin_addr.s_addr
-                       << " guiaddr: " << gui_address.sin_addr.s_addr << std::endl;
+    init_hints(addr_hints_gui, IPPROTO_TCP, SOCK_STREAM);
+    if (getaddrinfo(gui_addr_arg, gui_port, &addr_hints_gui, &addr_gui_result) != 0)
+        syserr("getaddrinfo gui");
 
-    server_sock = socket(PF_INET6, SOCK_DGRAM, 0);
-    if (server_sock < 0)
-        syserr("socket server");
-    gui_sock = socket(PF_INET6, SOCK_STREAM, 0);
+    gui_sock = socket(addr_gui_result->ai_family,
+                      addr_gui_result->ai_socktype,
+                      addr_gui_result->ai_protocol);
     if (gui_sock < 0)
         syserr("socket gui");
 
-    sendto(server_sock, player_name.c_str(), player_name.length(), 0,
-           (struct sockaddr *)&server_address, (socklen_t) sizeof(server_address));
+    if (connect(gui_sock, addr_gui_result->ai_addr, addr_gui_result->ai_addrlen))
+        syserr("connect gui");
+    
+    freeaddrinfo(addr_gui_result);
 
     return 123;
 };
